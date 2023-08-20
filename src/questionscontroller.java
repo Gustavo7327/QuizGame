916448cc36515e502e9c311e467381e7c9e740c6
Gustavo7327 <gustavosilva712493@gmail.com>
2023-08-20 18:39:26 -0300

    ...^-^

---
+++ src/questionscontroller.java
@@ -0,0 +1,43 @@
+package src;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.AnchorPane;
+
+public class questionscontroller {
+
+    @FXML
+    private Button buttonverificar;
+
+    @FXML
+    private Label labelpergunta;
+
+    @FXML
+    private AnchorPane panequestion;
+
+    @FXML
+    private RadioButton resposta1;
+
+    @FXML
+    private RadioButton resposta2;
+
+    @FXML
+    private RadioButton resposta3;
+
+    @FXML
+    private RadioButton resposta4;
+
+    @FXML
+    void displayoptions(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verificar(ActionEvent event) {
+
+    }
+
+}
