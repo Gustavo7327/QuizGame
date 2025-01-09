57dc9083520ab8310d1c83937fba53a6441d85cc
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-01-09 18:04:40 -0300

    feat: add image animation and restart method

--- quiz-game/src/main/java/quiz/game/quizgame/controllers/ResultController.java
+++ quiz-game/src/main/java/quiz/game/quizgame/controllers/ResultController.java
@@ -1,12 +1,15 @@
 package quiz.game.quizgame.controllers;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,9 +19,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class ResultController implements Initializable{
 
@@ -45,6 +50,24 @@ public class ResultController implements Initializable{
 
     @FXML
     void nextQuestion(ActionEvent event) {
+        loadScreen(event);
+    }
+
+    @FXML
+    void restartQuiz(ActionEvent event) {
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter("src/main/java/quiz/game/quizgame/controllers/control.csv"));
+            String newValues = 0 + "," + 0 + "," + false;
+            bw.write(newValues);
+            bw.flush();
+            bw.close();
+            loadScreen(event);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void loadScreen(ActionEvent event){
         try {
             root = FXMLLoader.load(getClass().getResource("../fxmls/Quiz.fxml"));
         } catch (IOException e) {
@@ -56,26 +79,41 @@ public class ResultController implements Initializable{
         stage.show();
     }
 
-    @FXML
-    void restartQuiz(ActionEvent event) {
-        System.out.println("funciona");
-    }
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
             br = new BufferedReader(new FileReader("src/main/java/quiz/game/quizgame/controllers/control.csv"));
             String line = br.readLine();
             String[] params = line.split(",");
+            int current = Integer.parseInt(params[0]);
             boolean correct = Boolean.parseBoolean(params[2]);
-            if(correct) result.setText("Acertou");  
-            else result.setText("Errou");        
+            if(correct){
+                imageContainer.setImage(new Image(getClass().getResourceAsStream("/quiz/game/quizgame/resources/imgs/sabo.jpeg")));
+                result.setText("Acertou! Pontuação: "+params[1]);  
+            } 
+            else{
+                imageContainer.setImage(new Image(getClass().getResourceAsStream("/quiz/game/quizgame/resources/imgs/burro.jpeg")));
+                result.setText("Errou! Pontuação: "+params[1]); 
+            }  
+            if (current == 12) {
+                buttonContainer.getChildren().remove(next);
+            }      
             br.close();
+            animarImagem();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void animarImagem() {
+        FadeTransition fadeTransition = new FadeTransition();
+        fadeTransition.setNode(imageContainer);
+        fadeTransition.setDuration(Duration.seconds(2.5)); 
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        fadeTransition.play();
     }
 
 }
